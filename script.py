38107b1bef463543d2bb6eb935d026e146ed80a0
Tobias Kapp <kapp.tob@gmail.com>
2023-11-09 10:58:28 +0100

    main eingeführt

--- script.py
+++ script.py
@@ -1,5 +1,17 @@
+import schedule
+import time
+
 from google_calendar_package.calendar_controller import getViableEvents
 from mqtt_package.mqtt_publisher import send_events
 
-events = getViableEvents()
-send_events(events)
+
+def start_google_events():
+    events = getViableEvents()
+    send_events(events)
+
+if __name__ == '__main__':
+    schedule.every().day.at("01:00").do(start_google_events)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
